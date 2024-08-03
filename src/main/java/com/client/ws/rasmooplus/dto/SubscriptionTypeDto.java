dc832c8255318ba97e6cf5823387ec1487983a9e
Guilherme Ribeiro <ribeiro.guilhermedesouza@gmail.com>
2024-08-03 00:53:12 -0300

    use jakart validation to valid dto fields and handle errors

--- src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
+++ src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
@@ -1,5 +1,9 @@
 package com.client.ws.rasmooplus.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,11 +18,16 @@ import java.math.BigDecimal;
 public class SubscriptionTypeDto {
     private Long id;
 
+    @NotBlank(message = "must not be null or empty")
     private String name;
 
+    @Max(value = 12, message = "must not be higher than 12")
     private Long accessMonth;
 
+    @NotNull(message = "must not be null")
     private BigDecimal price;
 
+    @NotBlank(message = "must not be null or empty")
+    @Size(min = 5, max = 15, message = "must be between 5 and 15")
     private String productKey;
 }
--- src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -5,8 +5,14 @@ import com.client.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.rasmooplus.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ResourceHandler {
@@ -30,5 +36,23 @@ public class ResourceHandler {
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .statusCode(HttpStatus.BAD_REQUEST.value())
                 .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> methodsArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> messages = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(e -> {
+            String field = ((FieldError) e).getField();
+            String message = e.getDefaultMessage();
+            messages.put(field, message);
+        });
+
+        ErrorResponseDto dto = ErrorResponseDto.builder()
+                .message(Arrays.toString(messages.entrySet().toArray()))
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
     }
 }
